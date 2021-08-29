ad6432fcd05b1f5408512a56b468dc51818abcfa
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-08-29 01:20:41 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Bot To Stream Musics on PyTGcalls with Channel Support.
+
+A Telegram Bot to Play Audio in Voice Chats With Supports Live streaming from youtube and Mega Radio Fm Streamings
+
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/shamilhabeebnelli/MwK-Musics/tree/main)
+
+## Deploy On Railway
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https://github.com/shamilhabeebnelli/mwk-musics/tree/main&envs=API_ID,API_HASH,BOT_TOKEN,ARQ_API,SESSION_STRING,CHAT,ADMINS,STREAM_URL,ADMIN_ONLY,DUR,LOG_GROUP,REPLY_MESSAGE&optionalEnvs=LOG_GROUP,ADMIN_ONLY,DUR,REPLY_MESSAGE&API_IDDesc=Your+App+id+and+hash+:get+it+from+my.telegram.org/apps&API_HASHDesc=Your+App+id+and+hash+:get+it+from+my.telegram.org/apps&BOT_TOKENDesc=Your+Bot+Api+Token+Get+it+from+@botfather&SESSION_STRINGDesc=your+pyrogram+session+string+:get+it+from+@gensessionbot&ARQ_APIDesc=your+ARQ+API+:get+it+from+@ARQROBOT&ADMINSDesc=your+telegram+ID+And+ids+of+whom+you+wish+to+controll+this&ADMIN_ONLYDesc=Optional+set+this+Y+or+N&LOG_GROUPDesc=Optional:+ID+of+your+bot+commands+log+group;+only+needed+if+you+are+playing+on+channel&REPLY_MESSAGEDesc=Optional+if+you+need+profile+guard+otherwise+leave+blank&CHATDesc=ID+of+channel/group+were+you+want+the+bot+user+to+play&STREAM_URLDesc=Stream+URL+of+radio+station+or+a+youtube+live+video+to+stream+when+the+bot+starts+or+with+/r+command&ADMIN_ONLYDefault=Y&STREAM_URLDefault=https://bit.ly/MwKradio&ADMINSDefault=749673781&DURDefault=300&REPLY_MESSAGEDefault=Hi+Iam+a+bot,+i+can+stream+audios+on+vc+😌&CHATDefault=-100)
+
+# Vars:
+1. `API_ID` : Get From my.telegram.org
+2. `API_HASH` : Get from my.telegram.org
+3. `BOT_TOKEN` : @Botfather
+4. `SESSION_STRING` : Generate From here [![GenerateStringName](https://img.shields.io/badge/Telegram-PyroSession-blueblack)](https://t.me/genstr_bot)
+5. `CHAT` : ID of Channel/Group where the bot plays Music.
+6. `LOG_GROUP` : Group to send Playlist, if CHAT is a Group
+7. `ADMINS` : ID of users who can use admin commands.
+8. `ARQ_API` : Get it for free from [@ARQRobot](https://telegram.dog/ARQRobot), This is required for /dplay to work.
+8. `STREAM_URL` : Stream URL of radio station or a youtube live video to stream when the bot starts or with /r  command.
---
+++ shamil/commands.py
@@ -0,0 +1,102 @@
+# Regen & Mod by @shamilhabeebnelli
+# Pyrogram - Telegram MTProto API Client Library for Python
+# Copyright (C) 2017-2020 Dan <https://github.com/delivrance>
+#
+# This file is part of Pyrogram.
+#
+# Pyrogram is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyrogram is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+import signal
+from utils import USERNAME, FFMPEG_PROCESSES
+from config import Config
+import os
+import sys
+U=USERNAME
+CHAT=Config.CHAT
+
+
+HOME_TEXT = "<b>Helo, [{}](tg://user?id={})\n\n• Iam A Bot Project by MwK MusicS\n• I Can Manage Group VC's\n\n• Hit /help to know about available commands.</b>"
+HELP = """
+🎧 <b>I Can Play Musics On VoiceChats 🤪</b>
+
+🎶 **Common Commands**:
+• `/song` __Download Song from youtube__
+• `/c`  __Show current playing song__
+• `/help` __Show help for commands__
+• `/mwk` __Shows the playlist__
+• `/stickerid` __To Get Id Of Replied Sticker__
+
+🎶 **Admin Commands**:
+• `/p`  __Reply to an audio file or YouTube link to play it or use /p <song name>__
+• `/d` __Play music from Deezer, Use /d <song name>__
+• `/sk [n]` __...Skip current or n where n >= 2__
+• `/j`  __Join voice chat__
+• `/l`  __Leave current voice chat__
+• `/mwk`  __Check which VC is joined__
+• `/sp`  __Stop playing__
+• `/r` __Start Radio__
+• `/sr` __Stops Radio Stream__
+• `/rp`  __Play from the beginning__
+• `/cl`  __Remove unused RAW PCM files__
+• `/ps` __Pause playing__
+• `/rs` __Resume playing__
+• `/m`  __Mute in VC__
+• `/um`  __Unmute in VC__
+• `/update` __Update Current Settings n Restarts the Bot__
+
+© Powered By 
+[ __@mwklinks | @redbullfed__ ]
+"""
+
+
+
+@Client.on_message(filters.command('start'))
+async def start(client, message):
+    buttons = [
+        [
+        InlineKeyboardButton("❔ How To Use Me ❔", callback_data="help"),
+                ],[
+                InlineKeyboardButton('📢 Updates', url='https://t.me/mwklinks'),
+                InlineKeyboardButton('💬 Support', url='https://t.me/redbullfed')
+                ],[
+                InlineKeyboardButton('🤖 Developer', url='https://t.me/shamilnelli'),
+                InlineKeyboardButton('🎧 Songs', url='https://t.me/joinchat/OsJr6i6C05E0NmQ1')
+                ],[
+                InlineKeyboardButton('📜 Source Code 📜', url='https://github.com/shamilhabeebnelli/mwk-musics'),
+    ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo="https://telegra.ph/file/a3937c3ddc19bb3300d89.jpg", caption=HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    await message.delete()
+
+
+@Client.on_message(filters.command("help"))
+async def show_help(client, message):
+    buttons = [
+        [
+                InlineKeyboardButton('📢 Updates', url='https://t.me/mwklinks'),
+                InlineKeyboardButton('💬 Support', url='https://t.me/redbullfed')
+                ],[
+                InlineKeyboardButton('🤖 Developer', url='https://t.me/shamilnelli'),
+                InlineKeyboardButton('🎧 Songs', url='https://t.me/joinchat/OsJr6i6C05E0NmQ1')
+                ],[
+                InlineKeyboardButton('📜 Source Code 📜', url='https://github.com/shamilhabeebnelli/mwk-musics'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply_photo(photo="https://telegra.ph/file/a3937c3ddc19bb3300d89.jpg", caption=HELP, reply_markup=reply_markup)
+    await message.delete()
